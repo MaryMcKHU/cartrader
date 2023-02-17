e3567c6acdadd849b127e9781271ac90c230daba
Mary McKinnon <mary.mckinnon@health-union.com>
2023-02-17 14:00:47 -0800

    Completed Section 5

---
+++ components/Car/Card.client.vue
@@ -0,0 +1,39 @@
+<script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+
+const props = defineProps({
+  car: Object,
+  favored: Boolean,
+})
+const emit = defineEmits(['favor'])
+// const favored = useState(`favored-${props.car.id}`, () => {
+//   return false
+// });
+</script>
+
+<template>
+  <div  class="relative shadow border w-full overflow-hidden mb-5 h-[200px]">
+    <img 
+      class="absolute w-7 right-5 top-2 z-20 cursor-pointer"
+      :src="favored ? heartFilled : heartOutline"
+      alt=""
+      @click="emit('favor', car.id)"
+    />
+    <div class="flex h-full">
+      <NuxtImg 
+        :src="car.url"
+        alt=""
+        class="w-[300px] h-full cursor-pointer"
+        @click="navigateTo(`/car/${car.name}-${car.id}`)"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="text-gray-700">{{ car.description  }}</p>
+        </div>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
