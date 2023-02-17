c15c607482e20e0e9cc7a68e87f9584b9a6a4868
Mary McKinnon <mary.mckinnon@health-union.com>
2023-02-16 16:01:35 -0800

    Section 2-14

---
+++ components/Car/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+    <div class="flex h-full">
+      <img 
+        src="https://hips.hearstapps.com/hmg-prod/images/2019-honda-civic-sedan-1558453497.jpg?resize=480:*"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">Volvo XC40</h1>
+          <p class="text-gray-700">Hello hello hello</p>
+        </div>
+        <h1 class="mt-auto text-xl">$40,000</h1>
+      </div>
+    </div>
+  </div>
+</template>
